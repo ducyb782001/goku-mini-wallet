d19c6c1c8ce88f1c54d57e8322fc43160f1d1208
ducndt-dgg <ducnd@dgg.network>
2024-10-01 15:06:35 +0700

    feat: add init data to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,31 @@
 "use client";
 
 import WebApp from "@twa-dev/sdk";
-import { useRouter } from "next-nprogress-bar";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      WebApp.ready();
-    }
-  }, []);
+  const [platForm, setPlatForm] = useState<any>();
+  const [initData, setInitData] = useState<any>();
+  const [initDataUnsafe, setInitDataUnsafe] = useState<any>();
 
   return (
-    <div className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-          onClick={handleClick}
-        /> */}
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-        <button className="bg-blue">Init data</button>
-        <button
-          className="bg-blue"
-          onClick={() => {
-            router.push("/photos");
-          }}
-        >
-          Go to photos
-        </button>
-      </main>
+    <div className="p-14">
+      <button
+        className="text-2xl font-bold bg-blue p-2 text-white rounded"
+        onClick={() => {
+          setPlatForm(WebApp.platform);
+          setInitData(WebApp.initData);
+          setInitDataUnsafe(WebApp.initDataUnsafe);
+        }}
+      >
+        Click to get init data
+      </button>
+      <h2 className="text-3xl font-bold mt-5">DATA:</h2>
+      <div className="mt-5 flex flex-col gap-5">
+        <div>platform:::🚀 {JSON.stringify(platForm)}</div>
+        <div>initData:::🚀 {JSON.stringify(initData)} </div>
+        <div>initDataUnsafe:::🚀 {JSON.stringify(initDataUnsafe)} </div>
+      </div>
     </div>
   );
 }
